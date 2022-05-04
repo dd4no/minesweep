0f66701dea77fdc62458e94dfc95fd7f2924a75d
Dave Defourneaux <dave.defourneaux@gmail.com>
2022-05-04 17:42:44 -0400

    Check for safe squares surrounding safe squares

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const isLeftEdge = (i % width === 0);
             const isRightEdge = (i % width === width - 1);
             let total = 0;
-
+            
             if (squares[i].classList.contains('safe')){
                 if (i>0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) total ++;
                 if (i>9 && !isRightEdge && squares[i+1-width].classList.contains('bomb')) total ++;
@@ -51,17 +51,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 squares[i].setAttribute('data', total)
             }
         }
-
-
+        
+        
     }
     createBoard();
-
+    
     // Square Choice Actions
     function chooseSquare(square) {
+        let number = square.id;
         // Check for previous clicks
         if (isGameOver) return;
         if (square.classList.contains('checked') || square.classList.contains('flag')) return;
-
+        
         // Check for Bomb
         if (square.classList.contains('bomb')) {
             isGameOver = true;
@@ -76,8 +77,58 @@ document.addEventListener('DOMContentLoaded', () => {
                 return;
             }
             else {
-                square.classList.add('checked');
+                checkSquare(square, number)
             }
+            square.classList.add('checked');
         }
+    }
+    
+    function checkSquare(square, number) {
+        const isLeftEdge = (number % width === 0);
+        const isRightEdge = (number % width === width - 1);
+
+        setTimeout(() => {
+            if (number > 0 && !isLeftEdge) {
+                const newNumber = squares[parseInt(number) - 1].id;
+                const newSquare = document.getElementById(newNumber);
+                chooseSquare(newSquare);
+            }
+            if (number > 9 && !isRightEdge) {
+                const newNumber = squares[parseInt(number) + 1 - width].id;
+                const newSquare = document.getElementById(newNumber);
+                chooseSquare(newSquare);
+            }
+            if (number > 10) {
+                const newNumber = squares[parseInt(number - width)].id;
+                const newSquare = document.getElementById(newNumber);
+                chooseSquare(newSquare);
+            }
+            if (number > 11 && !isLeftEdge) {
+                const newNumber = squares[parseInt(number) - 1 - width].id;
+                const newSquare = document.getElementById(newNumber);
+                chooseSquare(newSquare);
+            }
+            if (number < 98 && !isRightEdge) {
+                const newNumber = squares[parseInt(number) + 1].id;
+                const newSquare = document.getElementById(newNumber);
+                chooseSquare(newSquare);
+            }
+            if (number < 90 && !isLeftEdge) {
+                const newNumber = squares[parseInt(number) - 1 + width].id;
+                const newSquare = document.getElementById(newNumber);
+                chooseSquare(newSquare);
+            }
+            if (number < 88 && !isRightEdge) {
+                const newNumber = squares[parseInt(number) + 1 + width].id;
+                const newSquare = document.getElementById(newNumber);
+                chooseSquare(newSquare);
+            }
+            if (number < 89) {
+                const newNumber = squares[parseInt(number) + width].id;
+                const newSquare = document.getElementById(newNumber);
+                chooseSquare(newSquare);
+            }
+        }, 10);
+        
     }
 })
